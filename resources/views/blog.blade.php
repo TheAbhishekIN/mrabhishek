b71b581f19e9b839eac7dfd1ec85b8a9cbf50c2c
Abhishek Sharma <biz.abhisharma@gmail.com>
2024-09-01 20:13:17 +0530

    initial commit

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,3 @@
+<x-main-layout>
+    <h1 class="mb-8 text-2xl font-medium tracking-tighter">Coming Soon!</h1>
+</x-main-layout>
---
+++ resources/views/components/link.blade.php
@@ -0,0 +1,3 @@
+<a href="{{ $href }}" {{ $attributes->merge(['class' => 'text-blue-500 hover:text-blue-700']) }}>
+    {{ $slot }}
+</a>
